6d6da299acaba5859c9faec53e697a6ece4d953a
pastchick3 <pastchick3@gmail.com>
2022-08-02 15:21:52 +0900

    Support a few more functions

--- src/llvm_parser.rs
+++ src/llvm_parser.rs
@@ -54,7 +54,6 @@ pub fn parse(ir: &Vec<u8>, rv_prog: &Program) -> (HashMap<Address, String>, Stri
                     break;
                 }
             }
-
             if let Address(0) = addr {
             } else {
                 let func_ty = LLVMGetElementType(LLVMTypeOf(func));
@@ -79,7 +78,7 @@ pub fn parse(ir: &Vec<u8>, rv_prog: &Program) -> (HashMap<Address, String>, Stri
                         ) {
                             (32, 32) => {
                                 var_vec.push(None);
-                                let mut func_s = String::from("define i64 @f(i64 %target, %struct.reg* %reg, %struct.freg* %freg) {
+                                let mut func_s = format!("define i64 @{func_name}(i64 %target, %struct.reg* %reg, %struct.freg* %freg) {{
 entry:
   %arg_1_ptr = getelementptr %struct.reg, %struct.reg* %reg, i32 0, i32 10
   %arg_1 = load i64, i64* %arg_1_ptr
@@ -127,6 +126,219 @@ entry:
                             }
                             _ => None,
                         }
+                    }
+                    (LLVMTypeKind::LLVMVoidTypeKind, 0, []) => {
+                        let mut func_s = format!("define i64 @{func_name}(i64 %target, %struct.reg* %reg, %struct.freg* %freg) {{
+entry:
+  br label %0
+");
+                        let mut block = LLVMGetFirstBasicBlock(func);
+                        while {
+                            func_s += &format!("{}:\n", var_vec.len());
+                            var_vec.push(None);
+                            let mut inst = LLVMGetFirstInstruction(block);
+                            while {
+                                let inst_s = from_c_str(LLVMPrintValueToString(inst));
+                                if inst_s.starts_with("  %") {
+                                    var_vec.push(Some(inst));
+                                }
+                                match LLVMGetInstructionOpcode(inst) {
+                                    LLVMOpcode::LLVMRet => {
+                                        func_s.push_str(&format!(
+                                            "  %ra_{t} = getelementptr %struct.reg, %struct.reg* %reg, i32 0, i32 1
+  %ret_{t} = load i64, i64* %ra_{t}
+  ret i64 %ret_{t}"
+                                        ));
+                                        t += 1;
+                                    }
+                                    LLVMOpcode::LLVMStore => {
+                                        let val = LLVMGetOperand(inst, 0);
+                                        let val_ty =
+                                            from_c_str(LLVMPrintTypeToString(LLVMTypeOf(val)));
+                                        let val_s = {
+                                            let s = from_c_str(LLVMPrintValueToString(val));
+                                            if s.starts_with("  %") {
+                                                format!(
+                                                    "{val_ty} {}",
+                                                    s.trim().split(' ').next().unwrap()
+                                                )
+                                            } else {
+                                                s
+                                            }
+                                        };
+                                        let dest = LLVMGetOperand(inst, 1);
+                                        let mut len = 0;
+                                        let dest_name =
+                                            from_c_str(LLVMGetValueName2(dest, &mut len));
+                                        let dest_i = rv_prog
+                                            .data_blocks
+                                            .iter()
+                                            .position(|b| b.symbol == dest_name)
+                                            .unwrap();
+                                        let Address(dest_addr) =
+                                            rv_prog.data_blocks[dest_i].address;
+                                        let data_len = 141912; // Fix me
+                                        func_s.push_str(&format!(
+                                            "  %ptr_{t} = getelementptr [{data_len} x i8], [{data_len} x i8]* @data_0, i64 0, i64 {dest_addr}
+  %cast_ptr_{t} = bitcast i8* %ptr_{t} to {val_ty}*
+  store {val_s}, {val_ty}* %cast_ptr_{t}
+"
+                                        ));
+                                        t += 1;
+                                    }
+                                    LLVMOpcode::LLVMLoad => {
+                                        let i =
+                                            var_vec.iter().position(|i| i == &Some(inst)).unwrap();
+                                        let ty =
+                                            from_c_str(LLVMPrintTypeToString(LLVMTypeOf(inst)));
+                                        let src = LLVMGetOperand(inst, 0);
+                                        let mut len = 0;
+                                        let src_name = from_c_str(LLVMGetValueName2(src, &mut len));
+                                        let src_i = rv_prog
+                                            .data_blocks
+                                            .iter()
+                                            .position(|b| b.symbol == src_name)
+                                            .unwrap();
+                                        let Address(src_addr) = rv_prog.data_blocks[src_i].address;
+                                        let data_len = 141912; // Fix me
+                                        func_s.push_str(&format!(
+                                            "  %ptr_{t} = getelementptr [{data_len} x i8], [{data_len} x i8]* @data_0, i64 0, i64 {src_addr}
+  %cast_ptr_{t} = bitcast i8* %ptr_{t} to {ty}*
+  %{i} = load {ty}, {ty}* %cast_ptr_{t}
+"
+                                        ));
+                                        t += 1;
+                                    }
+                                    _ => func_s.push_str(&inst_s),
+                                }
+                                func_s.push('\n');
+                                inst = LLVMGetNextInstruction(inst);
+                                !inst.is_null()
+                            } {}
+                            block = LLVMGetNextBasicBlock(block);
+                            !block.is_null()
+                        } {}
+                        func_s.push_str("}\n");
+                        Some(func_s)
+                    }
+                    (LLVMTypeKind::LLVMVoidTypeKind, 1, [LLVMTypeKind::LLVMPointerTypeKind]) => {
+                        var_vec.push(None);
+                        let data_len = 141912; // Fix me
+                        let mut func_s = format!("define i64 @{func_name}(i64 %target, %struct.reg* %reg, %struct.freg* %freg) {{
+entry:
+  %arg_1_ptr = getelementptr %struct.reg, %struct.reg* %reg, i32 0, i32 10
+  %arg_1 = load i64, i64* %arg_1_ptr
+  %host_ptr_1 = getelementptr [{data_len} x i8], [{data_len} x i8]* @data_0, i64 0, i64 %arg_1
+  %0 = bitcast i8* %host_ptr_1 to i32*
+  br label %1
+");
+                        let mut block = LLVMGetFirstBasicBlock(func);
+                        while {
+                            func_s += &format!("{}:\n", var_vec.len());
+                            var_vec.push(None);
+                            let mut inst = LLVMGetFirstInstruction(block);
+                            while {
+                                let inst_s = from_c_str(LLVMPrintValueToString(inst));
+                                if inst_s.starts_with("  %") {
+                                    var_vec.push(Some(inst));
+                                }
+                                match LLVMGetInstructionOpcode(inst) {
+                                    LLVMOpcode::LLVMRet => {
+                                        func_s.push_str(&format!(
+                                            "  %ra_{t} = getelementptr %struct.reg, %struct.reg* %reg, i32 0, i32 1
+  %ret_{t} = load i64, i64* %ra_{t}
+  ret i64 %ret_{t}"
+                                        ));
+                                        t += 1;
+                                    }
+                                    LLVMOpcode::LLVMStore => {
+                                        let val = LLVMGetOperand(inst, 0);
+                                        let val_ty =
+                                            from_c_str(LLVMPrintTypeToString(LLVMTypeOf(val)));
+                                        let val_s = {
+                                            let s = from_c_str(LLVMPrintValueToString(val));
+                                            if s.starts_with("  %") {
+                                                format!(
+                                                    "{val_ty} {}",
+                                                    s.trim().split(' ').next().unwrap()
+                                                )
+                                            } else {
+                                                s
+                                            }
+                                        };
+                                        let dest = LLVMGetOperand(inst, 1);
+                                        let mut len = 0;
+                                        let dest_name =
+                                            from_c_str(LLVMGetValueName2(dest, &mut len));
+                                        if !dest_name.is_empty() {
+                                            let dest_i = rv_prog
+                                                .data_blocks
+                                                .iter()
+                                                .position(|b| b.symbol == dest_name)
+                                                .unwrap();
+                                            let Address(dest_addr) =
+                                                rv_prog.data_blocks[dest_i].address;
+                                            let data_len = 141912; // Fix me
+                                            func_s.push_str(&format!(
+                                            "  %ptr_{t} = getelementptr [{data_len} x i8], [{data_len} x i8]* @data_0, i64 0, i64 {dest_addr}
+  %cast_ptr_{t} = bitcast i8* %ptr_{t} to {val_ty}*
+  store {val_s}, {val_ty}* %cast_ptr_{t}
+"
+                                        ));
+                                            t += 1;
+                                        } else {
+                                            let s = from_c_str(LLVMPrintValueToString(dest));
+                                            let dest_name =
+                                                s.trim().split(' ').collect::<Vec<_>>()[1];
+                                            func_s.push_str(&format!(
+                                                "  store {val_s}, {val_ty}* {dest_name}"
+                                            ));
+                                        }
+                                    }
+                                    LLVMOpcode::LLVMLoad => {
+                                        let i =
+                                            var_vec.iter().position(|i| i == &Some(inst)).unwrap();
+                                        let ty =
+                                            from_c_str(LLVMPrintTypeToString(LLVMTypeOf(inst)));
+                                        let src = LLVMGetOperand(inst, 0);
+                                        let mut len = 0;
+                                        let src_name = from_c_str(LLVMGetValueName2(src, &mut len));
+                                        if !src_name.is_empty() {
+                                            let src_i = rv_prog
+                                                .data_blocks
+                                                .iter()
+                                                .position(|b| b.symbol == src_name)
+                                                .unwrap();
+                                            let Address(src_addr) =
+                                                rv_prog.data_blocks[src_i].address;
+                                            let data_len = 141912; // Fix me
+                                            func_s.push_str(&format!(
+                                                "  %ptr_{t} = getelementptr [{data_len} x i8], [{data_len} x i8]* @data_0, i64 0, i64 {src_addr}
+      %cast_ptr_{t} = bitcast i8* %ptr_{t} to {ty}*
+      %{i} = load {ty}, {ty}* %cast_ptr_{t}
+    "
+                                            ));
+                                            t += 1;
+                                        } else {
+                                            let s = from_c_str(LLVMPrintValueToString(src));
+                                            let src_name =
+                                                s.trim().split(' ').collect::<Vec<_>>()[1];
+                                            func_s.push_str(&format!(
+                                                "  %{i} = load {ty}, {ty}* {src_name}"
+                                            ));
+                                        }
+                                    }
+                                    _ => func_s.push_str(&inst_s),
+                                }
+                                func_s.push('\n');
+                                inst = LLVMGetNextInstruction(inst);
+                                !inst.is_null()
+                            } {}
+                            block = LLVMGetNextBasicBlock(block);
+                            !block.is_null()
+                        } {}
+                        func_s.push_str("}\n");
+                        Some(func_s)
                     }
                     _ => None,
                 };
